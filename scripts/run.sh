8e3c4dbc121bc8a56a01675680f542f16a1934da
Álvaro Candela <alvarocandela47@gmail.com>
2025-06-19 20:38:03 +0200

    First commit

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+xrdb merge ~/.Xresources
+xrandr --output HDMI-A-0 --mode 2560x1080 --rate 100
+feh --bg-fill ~/Pictures/wall/dnord4k_dark.png &
+xset r rate 200 50 &
+rclone mount OneDrive\ UEx: ~/OneDrive --vfs-cache-mode writes &
+setxkbmap es
+numlockx
+picom &
+
+dash ~/.config/chadwm/scripts/bar.sh &
+while type chadwm >/dev/null; do chadwm && continue || break; done
