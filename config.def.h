cfa1898641acaa1f4ba58f00cc127b7b54291b69
Álvaro Candela <alvarocandela47@gmail.com>
2025-11-18 21:07:04 +0100

    Updated dwm config

--- config.def.h
+++ config.def.h
@@ -1,12 +1,13 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:style:medium:size=19" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -16,6 +17,14 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+static const char *const autostart[] = {
+  "feh", "--bg-fill", "/home/alvaro/.dots/config/wall/dnord4k_dark.png", NULL,
+  "xrandr", "--output", "HDMI-1", "--mode", "2560x1080", "--rate", "100", "--right-of", "eDP-1", NULL,
+  "xset", "s", "off", NULL,
+  "picom", NULL,
+	NULL /* terminate */
 };
 
 /* tagging */
@@ -45,8 +54,7 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
-/* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -58,44 +66,77 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *upvol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL};
+static const char *downvol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL};
+static const char *mutevol[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL};
+static const char *mutemic[] = { "pactl", "set-source-volume", "0", "toggle", NULL};
+static const char *brightup[] = { "brightnessctl", "set", "5%+", NULL};
+static const char *brightdown[] = { "brightnessctl", "set", "5%-", NULL};
+static const char *nextsong[] = { "playerctl", "next", NULL};
+static const char *prevsong[] = { "playerctl", "previous", NULL};
+static const char *playpause[] = { "playerctl", "playpause", NULL};
+
+static const char *roficmd[]  = { "~/.config/rofi/bin/launcher", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *powermenu[]  = { "~/.config/rofi/bin/powermenu", NULL };
+static const char *filemanager[] = { "st", "-e", "spf", NULL};
+static const char *pdfreader[] = { "zathura", NULL};
+static const char *webbrowser[] = { "launch-profiled-firefox", NULL};
+static const char *whatsapp[] = { "zapzap", NULL};
+static const char *music[] = { "spotify", NULL};
+static const char *IDE[] = { "emacsclient", "-c", NULL};
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier                     key                       function        argument */
+  {0,                             XF86XK_AudioLowerVolume,  spawn,           {.v = downvol}},
+  {0,                             XF86XK_AudioRaiseVolume,  spawn,           {.v = upvol}},
+  {0,                             XF86XK_AudioMute,         spawn,           {.v = mutevol}},
+  {0,                             XF86XK_AudioMicMute,      spawn,           {.v = mutemic}},
+  {0,                             XF86XK_AudioNext,         spawn,           {.v = nextsong}},
+  {0,                             XF86XK_AudioPrev,         spawn,           {.v = prevsong}},
+  {0,                             XF86XK_AudioPlay,         spawn,           {.v = playpause}},
+  {0,                             XF86XK_MonBrightnessUp,   spawn,           {.v = brightup}},
+  {0,                             XF86XK_MonBrightnessDown, spawn,           {.v = brightdown}},
+
+
+	{ MODKEY,                       XK_space,                 spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_Return,                spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_p,                     spawn,          {.v = powermenu } },
+	{ MODKEY,                       XK_r,                     spawn,          {.v = filemanager } },
+	{ MODKEY,                       XK_z,                     spawn,          {.v = pdfreader } },
+	{ MODKEY,                       XK_f,                     spawn,          {.v = webbrowser } },
+	{ MODKEY,                       XK_w,                     spawn,          {.v = whatsapp } },
+	{ MODKEY,                       XK_s,                     spawn,          {.v = music } },
+	{ MODKEY,                       XK_e,                     spawn,          {.v = IDE } },
+	{ MODKEY,                       XK_j,                     focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,                     focusstack,     {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_h,                     setmfact,       {.f = -0.05} },
+	{ MODKEY|Mod1Mask,              XK_l,                     setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Tab,                   view,           {0} },
+  { MODKEY|ShiftMask,             XK_j,                     zoom,           {0} },
+  { MODKEY|ShiftMask,             XK_k,                     zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,                     killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_t,                     setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,             XK_f,                     setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_m,                     setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ControlMask,           XK_space,                 setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,                 togglefloating, {0} },
+	{ MODKEY,                       XK_0,                     view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                     tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_h,                     focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_l,                     focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_h,                     tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_l,                     tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                                     0)
+	TAGKEYS(                        XK_2,                                     1)
+	TAGKEYS(                        XK_3,                                     2)
+	TAGKEYS(                        XK_4,                                     3)
+	TAGKEYS(                        XK_5,                                     4)
+	TAGKEYS(                        XK_6,                                     5)
+	TAGKEYS(                        XK_7,                                     6)
+	TAGKEYS(                        XK_8,                                     7)
+	TAGKEYS(                        XK_9,                                     8)
+	{ MODKEY|Mod1Mask,              XK_q,                     quit,           {0} },
 };
 
 /* button definitions */
